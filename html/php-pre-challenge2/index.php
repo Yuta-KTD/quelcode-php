e3631b4ec2cfcab1bae95622eff205e35db166d9
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-06-05 14:20:42 +0900

    バブルソートによる並べ替えを行いました。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,23 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
+// バブルソート
+// 参考：https://mikaduki.info/webprogram/php/1120/
 
+//並べ替え自体を行うセット回数
+for ($i = 0; $i < count($array); $i++)
+{
+    $next = count($array) - $i;
+    //並べ替えセット一回で行う内部の並べ替え回数
+    for ($j=1; $j < $next; $j++) {
+        if($array[$j] < $array[$j-1]) {
+            $swap = $array[$j];
+            $array[$j] = $array[$j-1];
+            $array[$j-1] = $swap;
+        }
+    }
 }
+
 // 修正はここまで
 
 echo "<pre>";

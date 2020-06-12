efdb4d0b9b3b8b9d6796fefce75e92306dda3244
Yuta KTD <61367978+Yuta-KTD@users.noreply.github.com>
2020-06-12 13:41:39 +0900

    01等が弾かれる正規表現の追加と、インデントの修正。

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -3,14 +3,14 @@ $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
 try {
-    $db = new PDO($dsn , $dbuser , $dbpassword);
+    $db = new PDO($dsn,$dbuser,$dbpassword);
 } catch (PDOException $e){
     http_response_code(500);
     echo '[' . http_response_code() . ']';    
 }
 
 $limit = $_GET['target'];
-if (preg_match("/^[0-9]+$/",$limit) || $limit >= 1) {
+if (preg_match("/^[0-9]+$/",$limit) && !preg_match("/^[0]/",$limit) && $limit >= 1) {
     $limit = (int)$limit;
 }else {
     http_response_code(400);
@@ -29,22 +29,22 @@ foreach ($tempValues as $makeInt) {
 //array_unshiftにて$arrsに切り取ったものを挿入することを繰り返している？
 function combination($array,$extract){
     $arrayCount = count($array);    
-    if($arrayCount<$extract){
-      return;
-    }elseif($extract==1){
-      for($i=0;$i<$arrayCount;$i++){
+    if($arrayCount < $extract) {
+        return;
+    }elseif($extract === 1){
+        for($i = 0;$i < $arrayCount;$i++){
         $arrs[$i]=array($array[$i]);
-      }
-    }elseif($extract>1){
-      $j=0;
-      for($i=0;$i<$arrayCount-$extract+1;$i++){
-        $ts=combination(array_slice($array,$i+1),$extract-1);
-        foreach($ts as $t){
-          array_unshift($t,$array[$i]);
-          $arrs[$j]=$t;
-          $j++;
         }
-      }
+    }elseif($extract > 1){
+        $j = 0;
+        for($i = 0;$i<$arrayCount - $extract + 1;$i++){
+            $ts=combination(array_slice($array,$i + 1),$extract - 1);
+            foreach($ts as $t){
+            array_unshift($t,$array[$i]);
+            $arrs[$j] = $t;
+            $j++;
+            }
+        }
     }
     return $arrs;
 }
@@ -53,14 +53,14 @@ $valueCount = count($values);
 //空のcombinationsを用意
 $combinations = array();
 //抜き取りは１つ以上以下存在しないので$i = 1にてcombinarion関数を使う
-for($i = 1; $i <= $valueCount; $i++) {
+for($i = 1;$i <= $valueCount;$i++) {
     $temps = combination($values,$i);
-        foreach($temps as $temp) {
-            if(array_sum($temp) === $limit) {
-                //参考資料だとimplodeだが、int型なのでarray_push→これによって組み合わせを包括した組み合わせができる
-                array_push($combinations, $temp);
-            }
+    foreach($temps as $temp) {
+        if(array_sum($temp) === $limit) {
+            //参考資料だとimplodeだが、int型なのでarray_push→これによって組み合わせを包括した組み合わせができる
+            array_push($combinations,$temp);
         }
+    }
 }
 
 echo json_encode($combinations);
